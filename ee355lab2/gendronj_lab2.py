70580bd891f4c2337f9a444138fa2ee8704ae247
Gendronja <42985401+Gendronja@users.noreply.github.com>
2019-01-18 02:47:00 -0800

    Update gendronj_lab2.py

--- ee355lab2/gendronj_lab2.py
+++ ee355lab2/gendronj_lab2.py
@@ -1,43 +1,41 @@
 # Matrix Multiplication
-c = 0
 
-# Matrix intake
+# Matrix file intake
 f = open("input.txt", "r")
 line = f.readline()
 row_count = 0
 
-# Count how many rows in matrix file
+# Count how many rows in matrix file to determine size of matrices
 while line:
 	line = f.readline()
 	row_count += 1
 size = row_count / 2
-size2 = size * size
 
 A = []
 B = []
 C = []
 
+# Reopen file to start at beginning
 f = open("input.txt", "r")
 
 # Input matrix data
 line = list(f)
-	
 for i in range(int(size)):
      A.append(line[i].split())
 
 for i in range(int(size)):
 	B.append(line[i+8].split())
  
-
 for i in range(int(size)):
 	for j in range(int(size)):
 		for k in range(int(size)):
 			c = c + (int(A[i][k]) * int(B[k][j]))
 		C.append(c)
 		c = 0
+
 count = 0
 outfile = open("outfile.txt", "w")
-for i in range(int(size2)):
+for i in range(int(size*size)):
 	outfile.write("%d " %(C[i]))
 	count += 1
 	if count == 8:
